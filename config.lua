bba227e28eca4f400836af37d497d6aeffc4e395
Jguer <joaogg3@gmail.com>
2017-06-22 22:58:30 +0100

    Added comments to config

--- config.lua
+++ config.lua
@@ -1,8 +1,12 @@
 #!/usr/bin/env lua
 style = {
+    -- Controls opacity of window
     opacity = 0.5;
+    -- Controls background color of screen
     bgcolor = "0,0,0";
+    -- Icon theme
     theme = "simplistic";
+    -- Each item is a button, "Label, Icon, Shortcut Key, Command"
     buttons = { "Cancel,cancel,q,:cancel",
         "Lock,lock,l,i3lock",
         "Suspend,suspend,s,systemctl start suspend",
